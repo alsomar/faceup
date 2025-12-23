5a763cca917de65fd78e5c39efefdb27690ffe69
Alejandro Soriano <hola@alejandrosoriano.xyz>
2025-12-23 11:46:21 +0100

    removed def self.extension

--- src/asm_faceup.rb
+++ src/asm_faceup.rb
@@ -5,10 +5,10 @@ module ASM_Extensions
   module FaceUp
 
     unless file_loaded?(__FILE__)
-      extension_id  = File.basename(__FILE__, '.*')
-      extension_dir = File.join(__dir__, extension_id)
-      loader      = File.join(extension_dir, 'main')
-      info_file   = File.join(extension_dir, 'info.json')
+      ext_id      = File.basename(__FILE__, '.*')
+      ext_dir     = File.join(__dir__, ext_id)
+      loader      = File.join(ext_dir, 'main')
+      info_file   = File.join(ext_dir, 'info.json')
       info_hash   = JSON.parse(File.read(info_file), symbolize_names: true)
 
       EXTENSION   = info_hash.freeze
@@ -19,18 +19,14 @@ module ASM_Extensions
       creator     = EXTENSION[:creator]
       copyright   = EXTENSION[:copyright]
 
-      @ext = SketchupExtension.new(name, loader)
-      @ext.version     = version
-      @ext.description = description
-      @ext.creator     = creator
-      @ext.copyright   = copyright
+      ext = SketchupExtension.new(name, loader)
+      ext.version     = version
+      ext.description = description
+      ext.creator     = creator
+      ext.copyright   = copyright
 
-      Sketchup.register_extension(@ext, true)
+      Sketchup.register_extension(ext, true)
       file_loaded(__FILE__)
-    end
-
-    def self.extension
-      @ext
     end
 
   end
