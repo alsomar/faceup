40c10363c9dae2426e953637b677cb786af3957d
Alejandro Soriano <hola@alejandrosoriano.xyz>
2026-05-19 19:14:13 +0200

    Update copyright range and last-update date

--- src/asm_faceup/config/environment.rb
+++ src/asm_faceup/config/environment.rb
@@ -5,7 +5,7 @@ module ASM_Extensions
     INFO_AUTHOR  = "Alejandro Soriano"
     INFO_VERSION = EXTENSION[:version].to_s.freeze
     INFO_UPDATE  = EXTENSION[:update].to_s.freeze
-    INFO_COPY    = "\u00A9 #{INFO_AUTHOR}, 2023-2024"
+    INFO_COPY    = "\u00A9 #{INFO_AUTHOR}, 2023-2026"
 
   end # module FaceUp
 end # module ASM_Extensions
