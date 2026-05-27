10af6b2e3e11f7d7fe80b2e001124c0cb96ff006
Alejandro Soriano <hola@alejandrosoriano.xyz>
2026-05-27 11:30:22 +0200

    added copyright range info

--- src/asm_faceup/config/environment.rb
+++ src/asm_faceup/config/environment.rb
@@ -5,7 +5,21 @@ module ASM_Extensions
     INFO_AUTHOR  = "Alejandro Soriano"
     INFO_VERSION = EXTENSION[:version].to_s.freeze
     INFO_UPDATE  = EXTENSION[:update].to_s.freeze
-    INFO_COPY    = "\u00A9 #{INFO_AUTHOR}, 2023-2026"
+    INFO_START   = EXTENSION[:dev_start].to_s.freeze
+
+    # Copyright range: dev_start year (fallback 2023) to current year
+    # (fallback "Now"), e.g. "2023-2026", "2026", or "2023-Now".
+    copy_start   = INFO_START[/\d{4}/]&.to_i || 2023
+    copy_year    = Time.now.year rescue nil
+    copy_range   =
+      if copy_year.nil?
+        "#{copy_start}-Now"
+      elsif copy_year > copy_start
+        "#{copy_start}-#{copy_year}"
+      else
+        copy_start.to_s
+      end
+    INFO_COPY    = "\u00A9 #{INFO_AUTHOR}, #{copy_range}".freeze
 
   end # module FaceUp
 end # module ASM_Extensions
