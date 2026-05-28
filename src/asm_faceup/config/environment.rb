9d8361ade53627e94febfa170d90121c22bf9eca
Alejandro Soriano <hola@alejandrosoriano.xyz>
2026-05-28 07:22:09 +0200

    Comments cleanup

--- src/asm_faceup/config/environment.rb
+++ src/asm_faceup/config/environment.rb
@@ -1,14 +1,14 @@
 module ASM_Extensions
   module FaceUp
 
+    # Metadata
     INFO_NAME    = "FaceUp"
     INFO_AUTHOR  = "Alejandro Soriano"
     INFO_VERSION = EXTENSION[:version].to_s.freeze
     INFO_UPDATE  = EXTENSION[:update].to_s.freeze
     INFO_START   = EXTENSION[:dev_start].to_s.freeze
 
-    # Copyright range: dev_start year (fallback 2023) to current year
-    # (fallback "Now"), e.g. "2023-2026", "2026", or "2023-Now".
+    # Copyright range: dev_start year to current year
     copy_start   = INFO_START[/\d{4}/]&.to_i || 2023
     copy_year    = Time.now.year rescue nil
     copy_range   =
@@ -19,6 +19,7 @@ module ASM_Extensions
       else
         copy_start.to_s
       end
+
     INFO_COPY    = "\u00A9 #{INFO_AUTHOR}, #{copy_range}".freeze
 
   end # module FaceUp
